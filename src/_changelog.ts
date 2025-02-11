2d59e6993cd308f3a1af29969108bc21722766dc
Nicholas Armstrong <nick@n15g.com>
2025-02-11 18:25:31 +1100

    * Added GitHub Actions CI build

--- src/_changelog.ts
+++ src/_changelog.ts
@@ -2,5 +2,6 @@ export const Changelog: { [id: string]: string } = {
     "2.0.0": "* Removed dependency on lodash.\n" +
         "* Moved from webpack to rollup for packaging.\n" +
         "* Change from GNU to The Unlicense.\n" +
-        "* Added eslint.\n",
+        "* Added eslint.\n" +
+        "* Added GitHub Actions for CI.\n",
 };
