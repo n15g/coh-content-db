b7935e72ab25492f54646af34cedd4d96b1236e4
Nicholas Armstrong <nick@n15g.com>
2025-03-31 20:26:19 +1100

    added mission requirement type

--- src/main/api/badge-requirement-type.ts
+++ src/main/api/badge-requirement-type.ts
@@ -2,6 +2,7 @@ export const BADGE_REQUIREMENT_TYPE = [
   'PLAQUE',
   'BADGE',
   'INVENTION',
+  'MISSION', // Complete a mission
   'ARC', // Complete a story arc
   'TASK_FORCE', // Complete a task force
   'INVENTION_PLUS_ONE', // Some invention badges require you to build x of two different invention levels, 'plus one of either level'.
