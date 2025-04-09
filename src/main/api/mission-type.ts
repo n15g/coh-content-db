31ebb63a38bc9ca40a02434198e976354c42ffa4
Nicholas Armstrong <nick@n15g.com>
2025-04-09 23:37:01 +1000

    Fixed whitespace in mission types.

--- src/main/api/mission-type.ts
+++ src/main/api/mission-type.ts
@@ -1,2 +1,2 @@
-export const MISSION_TYPE = ['story-arc', 'mission', 'task force', 'strike force', 'trial', 'personal-story'] as const
+export const MISSION_TYPE = ['story-arc', 'mission', 'task-force', 'strike-force', 'trial', 'personal-story'] as const
 export type MissionType = typeof MISSION_TYPE[number]
