31f2c8e82acfc5c712b4fa8303ba2547cf88f582
Nicholas Armstrong <nick@n15g.com>
2026-01-02 11:23:55 +1100

    Added list for extended morality values.

--- src/main/api/morality.ts
+++ src/main/api/morality.ts
@@ -1,31 +1,34 @@
 export const MORALITY = ['hero', 'vigilante', 'villain', 'rogue', 'resistance', 'loyalist'] as const
-export type Morality = typeof MORALITY[number]
-export type MoralityExtended = Morality
+export const MORALITY_EXTENDED = [
+  ...MORALITY,
   /**
    * Any of the Primal Earth moralities - Hero, Vigilante, Villain, Rogue.
    */
-  | 'primal'
+  'primal',
   /**
    * Either of the Praetorian Earth moralities - Resistance or Loyalist.
    */
-  | 'praetorian'
+  'praetorian',
   /**
    * The moralities that roll up to the Hero {@link Alignment} - Hero and Vigilante.
    */
-  | 'heroic'
+  'heroic',
   /**
    * The moralities that roll up to the Villain {@link Alignment} - Villain and Rogue.
    */
-  | 'villainous'
+  'villainous',
   /**
    * Moralities with access to Paragon City - Hero, Vigilante and Rogue.
    */
-  | 'paragon-city-access'
+  'paragon-city-access',
   /**
    * Moralities with access to the Rogue Isles - Villain, Rogue and Vigilante.
    */
-  | 'rogue-isles-access'
+  'rogue-isles-access',
   /**
    * All the moralities.
    */
-  | 'all'
+  'all',
+] as const
+export type Morality = typeof MORALITY[number]
+export type MoralityExtended = typeof MORALITY_EXTENDED[number]
